866488b8ad3fa44160ab56b0894079facf2cbd3c
conniechu929 <conniechu929@gmail.com>
2016-10-04 17:38:36 -0700

    added sorts

---
+++ bubble_sort.js
@@ -0,0 +1,14 @@
+function bubble_sort(array) {
+  for(var i = 0; i < array.length; i++) {
+    for(var j = i + 1; j < array.length; j++) {
+      if(array[i] > array[j]) {
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+  }
+  return array;
+};
+
+
