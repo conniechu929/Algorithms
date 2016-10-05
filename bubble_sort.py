7cc660440ed44a7ec41e418b12dba8ce946dac7d
conniechu929 <conniechu929@gmail.com>
2016-10-04 17:27:47 -0700

    Added start point for second loop

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,13 +1,9 @@
 def bubble_sort(list):
-	for i in range(len(list)):
-		for j in range(len(list)):
-			if list[i] < list[j]:
+	for i in range(len(list)-1):
+		for j in range(i + 1, len(list)):
+			if list[i] > list[j]:
 				(list[i], list[j]) = (list[j], list[i])
 	return list
 
 a = [9, 2, 0, 4, 3, 5, 8, 7, 9, 10, 30, 78,100, 20, 39, 3, 44, 5, 6]
 print bubble_sort(a)
-
-
- 
-
